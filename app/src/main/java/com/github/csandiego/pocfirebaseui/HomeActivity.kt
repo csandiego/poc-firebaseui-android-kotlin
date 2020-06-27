a0c211f4a2abee7113702e66d921e633c0e9d5f3
Christopher San Diego <csandiego@gmail.com>
2020-06-27 13:29:47 +0800

    Add Firebase

--- app/src/main/java/com/github/csandiego/pocfirebaseui/HomeActivity.kt
+++ app/src/main/java/com/github/csandiego/pocfirebaseui/HomeActivity.kt
@@ -2,29 +2,27 @@ package com.github.csandiego.pocfirebaseui
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     private val listener = FirebaseAuth.AuthStateListener { auth ->
         if (auth.currentUser == null) {
-            startActivityForResult(
-                AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(listOf(AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build())).build(),
-                100
-            )
-        } else {
-            startActivity(Intent(this, HomeActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
+        findViewById<Button>(R.id.logoutButton).setOnClickListener {
+            AuthUI.getInstance().signOut(this)
+        }
     }
-
     override fun onResume() {
         super.onResume()
         FirebaseAuth.getInstance().addAuthStateListener(listener)
